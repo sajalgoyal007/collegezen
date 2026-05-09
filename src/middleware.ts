b95c2c6f3b45599c90a99f3dbcf2d4b3d03c6553
Sajal Goyal <sajalgoyal2007@gmail.com>
2026-05-09 16:25:15 +0530

    Reduce middleware size

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,8 @@
-export { auth as middleware } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/saved/:path*", "/profile/:path*"],
